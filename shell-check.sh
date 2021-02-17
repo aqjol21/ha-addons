c360fbf47d20413adf8a0aec8ec2177efb08d230
aqjol <aertayuly@gmail.com>
2021-02-17 17:18:37 +0600

    Update shell-check.sh

--- shell-check.sh
+++ shell-check.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-docker run --rm -v "$(pwd)":/mnt:ro koalaman/shellcheck -- *.sh l2tp-client/*.sh zigbee2mqtt/*.sh
+docker run --rm -v "$(pwd)":/mnt:ro koalaman/shellcheck -- *.sh zigbee2mqtt/*.sh
